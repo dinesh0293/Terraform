7a70690e2d7239da68dd0cb4fb6a85cd14e2ff3e
dinesh0293 <dineshsampath93@gmail.com>
2020-08-14 23:49:07 +0530

    EC2 terraform file

---
+++ first_ec2.tf
@@ -0,0 +1,14 @@
+/*
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA3B5RYADHLYXZ6O6S"
+  secret_key = "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"
+}
+
+resource "aws_instance" "myec2" {
+  ami = "ami-02354e95b39ca8dec"
+  instance_type = "t2.micro"
+}
+
+
+*/
